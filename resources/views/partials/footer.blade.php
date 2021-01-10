4096e85de86b8922c469f8556b118451a219f5bd
alessio <alessio.cirillo.1995@gmail.com>
2021-01-10 21:55:03 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,158 @@
-<h1>footer</h1>
+<footer>
+    <div class="white-bar">
+
+    </div>
+    <div class="container-xl">
+        <div class="left-side">
+            <div class="footer-menu">
+                <div class="dc-db">
+                    <h2>
+                        dc comics
+                    </h2>
+                    
+                    <ul>
+                        <li>
+                            Chatacters
+                        </li>
+                        <li>
+                            Comics
+                        </li>
+                        <li>
+                            Movies
+                        </li>
+                        <li>
+                            TV
+                        </li>
+                        <li>
+                            Games
+                        </li>
+                        <li>
+                            Videos
+                        </li>
+                        <li>
+                            News
+                        </li>
+                    </ul>
+                    {{-- SHOP --}}
+                    <h2>
+                        shop
+                    </h2>
+    
+                    <ul>
+                        <li>
+                            shop DC
+                        </li>
+                        <li>
+                            shop DC collectibles
+                        </li>
+                    </ul>
+                </div>
+    
+                <div class="dc-network">
+                    <h2>
+                        dc
+                    </h2>
+                    
+                    <ul>
+                        <li>
+                           terms of use
+                        </li>
+                         <li>
+                            privacy police
+                        </li>
+                         <li>
+                            ad choise
+                        </li>
+                         <li>
+                            advertising
+                        </li>
+                         <li>
+                            jobs
+                        </li>
+                         <li>
+                            subscriptions
+                        </li>
+                         <li>
+                            talent workshop
+                        </li>
+                         <li>
+                            CPSC certificates
+                        </li>
+                         <li>
+                            rartings
+                        </li>
+                         <li>
+                            shop help
+                        </li>
+                         <li>
+                            contact us
+                        </li>
+                    </ul>
+                </div>
+    
+                <div class="sites">
+                    <h2>
+                        sites
+                    </h2>
+    
+                    <ul>
+                        <li>
+                            DC
+                        </li>
+                        <li>
+                            Mad Magazine
+                        </li>
+                        <li>
+                            DC kids
+                        </li>
+                        <li>
+                            DC universe
+                        </li>
+                        <li>
+                            DC power visa
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="copy">
+                <p>
+                    All Site Content TM and COPY 2020 DC Entertainment, unless otherwise <a href="">noted here</a>. All rights reserved.
+                    <a href="">Cookies Settings</a>
+                </p>
+            </div>
+        </div>
+    
+        <div class="footer-logo">
+            <img src="{{ asset('img/logo-footer.png') }}" alt="">
+        </div>
+    </div>
+
+    <div class="footer-nav">
+        <div class="container-xl">
+            <a href="">
+                <div class="button">
+                    sign-up now!
+                </div>
+            </a>
+    
+            <div class="social">
+                <h2>follow us</h2>
+                <ul>
+                    <li>
+                        <i class="fab fa-facebook-f"></i>
+                    </li>
+                    <li>
+                        <i class="fab fa-twitter"></i>
+                    </li>
+                    <li>
+                        <i class="fab fa-youtube"></i>
+                    </li>
+                    <li>
+                       <i class="fas fa-map-marker-alt"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
